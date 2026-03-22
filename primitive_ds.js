05fc0c0e37509e4a5a4b1bee52297cb5266fb07b
RashmiHG90 <rashmi.hguk@gmail.com>
2026-03-22 11:12:41 +0000

    primitive data tyepes example

---
+++ primitive_ds.js
@@ -0,0 +1,13 @@
+//create employee using primitive data types
+
+let id= 100;
+let fname = "Ravi";
+let age = 21;
+let isActive = true;
+let msg;
+
+console.log("id is " + id + "type of variable: " + typeof(id))
+console.log("first name is: "+ fname + "type of variable: " + typeof(fname))
+console.log("Age is: " + age + "type of variable: " + typeof(age))
+console.log("Is active :" + isActive + "type of variable: " + typeof(isActive));
+console.log(" Message value is: "+ msg + "type of variable: " + typeof(msg))
---
+++ welcome.js
@@ -0,0 +1,7 @@
+console.log("Welcome to Node JS Example")
+
+function Hello(){
+    console.log("JS function using node js")
+}
+
+Hello();
